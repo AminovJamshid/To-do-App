b486e50795253b721744f46e885dce23eaa3569e
AminovJamshid <aminovjamshid777gmail.com>
2024-08-05 22:14:08 +0500

    MINOR: fixed login  error

--- src/User.php
+++ src/User.php
@@ -6,6 +6,7 @@ class User
 {
     public function create()
     {
+
         if (isset($_POST['email']) && isset($_POST['password'])) {
             $email    = $_POST['email'];
             $password = $_POST['password'];
